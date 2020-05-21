20ac79a2667507a40df2c2d414e88c8422bec6f8
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-05-21 17:40:39 +0100

    Update licence.py
    Started using widgets.TextWindow to avoid code repetition

--- scr/licence.py
+++ scr/licence.py
@@ -1,18 +1,10 @@
-import tkinter as tk
+import widgets
 
 
-class Licence:
+class Licence(widgets.TextWindow):
     def __init__(self):
-        self.root = tk.Tk()
-        self.root.wm_attributes("-type", "splash")
-        self.text = tk.Text(self.root)
-        self.sbar = tk.Scrollbar(self.root, command=self.text.yview)
-        self.button = tk.Button(self.root, text="Ok", command=self.root.destroy)
-        self.text.insert("end", self.get_text())
-        self.text.config(state="disabled", yscrollcommand=self.sbar.set)
-        self.text.grid(row=1, column=1, sticky="news")
-        self.sbar.grid(row=1, column=2, sticky="news")
-        self.button.grid(row=2, column=1, columnspan=2, sticky="news")
+        super().__init__()
+        self.insert("end", self.get_text())
 
     def get_text(self):
         with open("Licence.txt", "r") as file:
