50966b34256fe56add97f3d44d71a2c2e6255436
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-05-23 18:58:42 +0100

    Added callback so that the GUIBoard knows when the computer's turn has finished

--- scr/computer.py
+++ scr/computer.py
@@ -3,16 +3,17 @@ from chess.engine import SimpleEngine, Limit, Mate
 
 
 class Computer(Player):
-    def __init__(self, board, colour, depth=None, time=2):
-        self.time = time
-        self.depth = depth
+    def __init__(self, board, colour, depth=None, time=2, callback=None):
+        self.callback = callback
         super().__init__(board=board, colour=colour)
-        self.limit = Limit(depth=self.depth, time=self.time)
+        self.limit = Limit(depth=depth, time=time)
 
     def go(self):
-        if (self.board.is_game_over()) or (not alowed_to_play):
+        if (self.board.is_game_over()) or (not self.alowed_to_play):
             return None
         with SimpleEngine.popen_uci("Stockfish/stockfish_10_x64") as engine:
             result = engine.play(self.board, self.limit)
         move = result.move
         self.board.push(move)
+        if self.callback is not None:
+            self.callback()
