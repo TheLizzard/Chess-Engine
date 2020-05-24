8ab935f0d0da322643dcdd483733efaf54d3e8e9
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-05-24 13:58:42 +0100

    Now the player isn't going to directly push a move to the board

--- scr/computer.py
+++ scr/computer.py
@@ -3,8 +3,10 @@ from chess.engine import SimpleEngine, Limit, Mate
 
 
 class Computer(Player):
-    def __init__(self, board, colour, depth=None, time=2, callback=None):
+    def __init__(self, board, colour, guiboard, depth=None, time=2,
+                 callback=None):
         self.callback = callback
+        self.guiboard = guiboard
         super().__init__(board=board, colour=colour)
         self.limit = Limit(depth=depth, time=time)
 
@@ -14,6 +16,6 @@ class Computer(Player):
         with SimpleEngine.popen_uci("Stockfish/stockfish_10_x64") as engine:
             result = engine.play(self.board, self.limit)
         move = result.move
-        self.board.push(move)
+        self.guiboard.push(move)
         if self.callback is not None:
             self.callback()
