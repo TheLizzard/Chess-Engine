6d9fa7bb2d5ba4c81238ee6e202e083caaff6268
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-05-25 19:28:49 +0100

    added a new connection to settings for STOCKFISH_LOCATION

--- scr/computer.py
+++ scr/computer.py
@@ -1,21 +1,24 @@
-from player import Player
 from chess.engine import SimpleEngine, Limit, Mate
+
+from player import Player
+import settings
+
+settings = settings.Settings()
+STOCKFISH_LOCATION = settings.evaluation.stockfish
+del settings
 
 
 class Computer(Player):
-    def __init__(self, board, colour, guiboard, depth=None, time=2,
-                 callback=None):
+    def __init__(self, board, colour, depth=None, time=2, callback=None):
         self.callback = callback
-        self.guiboard = guiboard
         super().__init__(board=board, colour=colour)
         self.limit = Limit(depth=depth, time=time)
 
     def go(self):
         if (self.board.is_game_over()) or (not self.alowed_to_play):
             return None
-        with SimpleEngine.popen_uci("Stockfish/stockfish_10_x64") as engine:
+        with SimpleEngine.popen_uci(STOCKFISH_LOCATION) as engine:
             result = engine.play(self.board, self.limit)
         move = result.move
-        self.guiboard.push(move)
         if self.callback is not None:
             self.callback()
