7212d1ff3bf42734d81660e2dc2aa94475e90f6e
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-05-25 19:30:15 +0100

    Added stockfish evaluation and suggested moves

--- scr/main.py
+++ scr/main.py
@@ -1,27 +1,36 @@
 from settings import Settings
 from board import GUIBoard
 import widgets
+from analyse import Analyse
 
 from functools import partial
 import tkinter as tk
+import threading
+import time
 
 
 class App:
     def __init__(self):
-        self.user_settings = Settings()
+        self.settings = Settings()
+        self.analysing = False
+        self.analyses = None
+        self.done_set_up = False
         self.set_up_tk()
         self.update_board()
+        self.done_set_up = True
         self.root.update()
+        self.start_analysing()
 
     def set_up_tk(self):
-        self.root = tk.Tk()
+        self.root = widgets.Tk()
+        self.root.bind_update(self.update)
         self.root.resizable(False, False)
         self.root.title("Chess.py")
         self.root.config(bg="black")
         self.set_up_menu()
         self.root.config(menu=self.menubar)
 
-        self.widget_kwargs = self.user_settings.widgets
+        self.widget_kwargs = self.settings.widgets
         self.modified_widget_kwargs = self.widget_kwargs.dict()
         self.justify = self.modified_widget_kwargs.pop("justify", None)
         self.set_up_board()
@@ -30,26 +39,26 @@ class App:
         self.set_up_movehistory()
 
     def set_up_menu(self):
-        tearoff = self.user_settings.menu.tearoff
+        tearoff = self.settings.menu.tearoff
         self.menubar = tk.Menu(self.root, tearoff=tearoff)
 
-        self.user_settings.menu.pop("tearoff")
+        self.settings.menu.pop("tearoff")
 
-        for name, _list in self.user_settings.menu.items():
+        for name, _list in self.settings.menu.items():
             name = name[0].upper()+name[1:]
-            self.menu_example = tk.Menu(self.menubar, tearoff=tearoff)
-            self.menubar.add_cascade(label=name, menu=self.menu_example)
+            self.submenu = tk.Menu(self.menubar, tearoff=tearoff)
+            self.menubar.add_cascade(label=name, menu=self.submenu)
             for button in _list:
                 if button.replace("-", "") == "":
-                    self.menu_example.add_separator()
+                    self.submenu.add_separator()
                     continue
                 button = button[0].upper()+button[1:]
                 event = (name+"."+button).lower().replace(" ", "_")
                 command = partial(self.menu, event)
-                self.menu_example.add_command(label=button, command=command)
+                self.submenu.add_command(label=button, command=command)
 
     def set_up_eval(self):
-        settings = self.user_settings.evaluation
+        settings = self.settings.evaluation
         width = settings.width
         height = settings.height
         fg = settings.colour
@@ -65,37 +74,33 @@ class App:
 
         self.eval_text = tk.Label(self.eval_frame, fg=fg, bg=bg, font=font,
                                   **self.modified_widget_kwargs)
-        self.eval_text.pack(side="left")
+        self.eval_text.grid(row=1, column=1, sticky="news")
+        self.eval_text.config(text="2.2")
 
     def set_up_suggestedmoves(self):
-        settings = self.user_settings.suggestedmoves
-        width = settings.width
-        height = settings.height
+        settings = self.settings.suggestedmoves
         fg = settings.colour
         bg = settings.background
         font = settings.font
-        self.suggestedmoves_frame = tk.Frame(self.root, width=width,
-                                             height=height, bg=bg,
-                                             **self.modified_widget_kwargs)
-        self.suggestedmoves_frame.grid(row=2, column=2)
-        self.suggestedmoves_frame.grid_propagate(False)
 
-        n_moves_text = tk.Label(self.suggestedmoves_frame, fg=fg, font=font,
-                                bg=bg, justify=self.justify,
-                                **self.modified_widget_kwargs)
-        n_moves_text.grid(row=1, column=1)
+        self.suggestedmoves_text = tk.Label(self.eval_frame, fg=fg, font=font,
+                                            bg=bg, justify=self.justify,
+                                            **self.modified_widget_kwargs)
+        self.suggestedmoves_text.grid(row=2, column=1, sticky="news")
+        self.suggestedmoves_text.config(text="No moves to suggest")
 
     def set_up_movehistory(self):
-        settings = self.user_settings.movehistory
+        settings = self.settings.movehistory
         width = settings.width
         height = settings.height
         fg = settings.colour
         bg = settings.background
+        cursor_colour = settings.cursor_colour
         font = settings.font
         self.movehistory_frame = tk.Frame(self.root, width=width,
                                           height=height, bg=bg,
                                           **self.modified_widget_kwargs)
-        self.movehistory_frame.grid(row=3, column=2)
+        self.movehistory_frame.grid(row=3, column=2, sticky="news")
 
         self.movehistory_frame.grid_propagate(False)
         self.movehistory_frame.pack_propagate(False)
@@ -106,6 +111,8 @@ class App:
 
         self.movehistory_text = tk.Text(self.movehistory_frame, height=height,
                                         width=width, font=font, fg=fg, bg=bg,
+                                        insertbackground=cursor_colour,
+                                        state="disabled",
                                         **self.modified_widget_kwargs)
 
         self.movehistory_text.pack(side="left", expand=True, fill="y")
@@ -114,7 +121,7 @@ class App:
                                      self.movehistory_text,
                                      command=self.movehistory_text.yview)
         self.movehistory_text["yscrollcommand"] = sbar.set
-        sbar.pack(side="right", expand=True, fill="y")=
+        sbar.pack(side="right", expand=True, fill="y")
 
     def menu(self, event):
         event = event.split(".")
@@ -177,20 +184,25 @@ class App:
 
     def start_game(self, event):
         if event == "evaluate":
+            self.start_analysing()
             print("game.evaluate")
 
         elif event == "play_vs_computer":
+            self.stop_analysing()
             colour = self.ask_if_user_white()
             self.board.start_comp_v_hum(1-colour)
 
         elif event == "play_vs_human":
+            self.stop_analysing()
             self.board.start_hum_v_hum()
 
         elif event == "play_vs_ai":
+            self.stop_analysing()
             colour = self.ask_if_user_white()
             self.board.start_ai_v_hum(1-colour)
 
         elif event == "play_multiplayer":
+            self.stop_analysing()
             print("game.play_multiplayer")
 
     def change_settings(self, event):
@@ -215,7 +227,10 @@ class App:
 
     def show_pgn(self):
         w = widgets.CopyableTextWindow()
-        w.set(self.board.pgn())
+        w.set(self.pgn())
+
+    def pgn(self):
+        return self.board.pgn()
 
     def show_licence(self):
         widgets.LicenceWindow()
@@ -224,12 +239,62 @@ class App:
         widgets.HelpWindow()
 
     def set_up_board(self):
-        self.board = GUIBoard(settings=self.user_settings.gameboard,
-                              root=self.root,
+        self.board = GUIBoard(settings=self.settings.gameboard,
+                              root=self.root, move_callback=self.moved,
                               kwargs=self.modified_widget_kwargs)
 
     def update_board(self):
         self.board.update()
 
+    def update(self):
+        if self.done_set_up and self.analysing:
+            if self.analyses is not None:
+                score = self.analyses.score
+                moves = self.analyses.moves
+
+                if (score is None) or (moves is None):
+                    return None
+
+                try:
+                    score = score.white().score(mate_score=10000)
+                    moves = self.board.moves_to_san(moves)[:4]
+
+                    self.eval_text.config(text=score)
+                    self.suggestedmoves_text.config(text=" ".join(moves))
+                except Exception as error:
+                    pass
+
+    def moved(self):
+        self.movehistory_text.config(state="normal")
+        old_pgn = self.movehistory_text.get("1.0", "end")[:-1]
+        new_pgn = self.pgn()
+        diff = self.find_diff_pgn(old_pgn, new_pgn)
+        self.movehistory_text.insert("end", diff)
+        self.movehistory_text.config(state="disabled")
+        self.analyses.kill()
+        self.start_analysing()
+
+    def find_diff_pgn(self, pgn1, pgn2):
+        if pgn1 == "\n":
+            pgn1 = ""
+        return pgn2[len(pgn1)-len(pgn2):][:-1]
+
+    def start_analysing(self):
+        self.analysing = True
+        self.analyses = Analyse(self.board.board)
+        thread = threading.Thread(target=self.analyses.start)
+        thread.deamon = True
+        thread.start()
+        self.eval_frame.grid()
+
+    def stop_analysing(self):
+        self.analysing = False
+        if self.analyses is not None:
+            self.analyses.kill()
+        self.eval_frame.grid_remove()
+
 
 a = App()
+while True:
+    a.root.update()
+    time.sleep(0.1)
