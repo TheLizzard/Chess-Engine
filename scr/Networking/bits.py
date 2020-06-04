1fe7d228d864d95d0fc8f828bf700512ea9f3644
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-06-04 21:34:07 +0100

    Added comments and method annotations

--- scr/Networking/bits.py
+++ scr/Networking/bits.py
@@ -1,57 +1,71 @@
+"""
+This module implements the Bits class. It uses a string as its value
+(not the best method).
+"""
+
+
 import struct
 
 
 class Bits:
-    def __init__(self, value=None):
+    def __init__(self, value: str=None):
         if value is None:
             value = ""
         assert isinstance(value, str)
         assert value.replace("1", "").replace("0", "") == ""
         self.value = value
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         if self.value == "":
             value = "None"
         else:
             value = self.value
         return "<Bits object at "+hex(id(self))+" value="+value+">"
 
-    def __getitem__(self, key):
+    def __getitem__(self, key) -> str:
         return Bits(self.value[key])
 
-    def __bool__(self):
-        return self.value != "0"
+    def __bool__(self) -> bool:
+        return (self.value != "0") and (self.value != "")
 
-    def __int__(self):
+    def __int__(self) -> int:
         return self.to_int()
 
-    def __bytes__(self):
+    def __bytes__(self) -> bytes:
         return self.to_bytes()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.value
 
     def __add__(self, other):
+        if not isinstance(other, self.__class__):
+            raise ValueError("Can't add Bits object with: "+repr(other))
         return Bits.from_int(int(self)+int(other))
 
     def __xor__(self, other):
+        if not isinstance(other, self.__class__):
+            raise ValueError("Can't xor Bits object with: "+repr(other))
         return Bits.from_int(int(self)^int(other))
 
     def __sub__(self, other):
+        if not isinstance(other, self.__class__):
+            raise ValueError("Can't subtract Bits object with: "+repr(other))
         return Bits.from_int(int(self)-int(other))
 
     def __mul__(self, other):
+        if not isinstance(other, self.__class__):
+            raise ValueError("Can't multiply Bits object with: "+repr(other))
         return Bits.from_int(int(self)*int(other))
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.value)
 
-    def to_int(self):
+    def to_int(self) -> int:
         if self.value == "":
             return 0
         return int(self.value, 2)
 
-    def to_bytes(self, errors="strict"):
+    def to_bytes(self, errors="strict") -> bytes:
         if (len(self)%8 != 0):
             if errors == "strict":
                 raise ValueError("bits not a multiple of 8")
@@ -68,14 +82,14 @@ class Bits:
         return Bits(self.value+other.value)
 
     @classmethod
-    def from_bytes(cls, _bytes):
+    def from_bytes(cls, _bytes: bytes):
         bits = cls()
         for i in _bytes:
             bits = bits.concatenate(cls.from_int(i, bits=8))
         return bits
 
     @classmethod
-    def from_int(cls, value, bits=None):
+    def from_int(cls, value: int, bits=None):
         if not isinstance(value, int):
             raise ValueError("value must be an int.")
         output = "{0:b}".format(value)
