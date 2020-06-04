55f6a475ca61b4c2e58d683207bc06a92dcb7715
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-06-04 15:31:07 +0100

    Standardised the board player protocol

--- scr/Players/computer.py
+++ scr/Players/computer.py
@@ -5,24 +5,19 @@ import settings
 
 settings = settings.Settings()
 STOCKFISH_LOCATION = settings.evaluation.stockfish
+DEPTH = settings["gameboard.computer"].depth
+TIME = settings["gameboard.computer"].time
 del settings
 
 
 class Computer(Player):
-    def __init__(self, board, colour, depth=None, time=2, callback=None):
-        self.callback = callback
-        self.depth = depth
-        self.time = time
-        super().__init__(board=board, colour=colour)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def go(self):
         if (self.board.is_game_over()) or (not self.alowed_to_play):
             return None
-        self.limit = Limit(depth=self.depth, time=self.time)
+        self.limit = Limit(depth=DEPTH, time=TIME)
         with SimpleEngine.popen_uci(STOCKFISH_LOCATION) as engine:
             result = engine.play(self.board, self.limit)
-        move = result.move
-        if self.callback is not None:
-            self.callback(move)
-        else:
-            return move
+        self.callback(result.move)
