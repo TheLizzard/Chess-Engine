d96c0ea3e9485222c7540753af29a634f21a0fd5
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-06-04 15:31:53 +0100

    Standardised the board player protocol

--- scr/Players/player.py
+++ scr/Players/player.py
@@ -1,19 +1,35 @@
-class Player:
-    def __init__(self, board, colour):
-        self.board = board
-        self.alowed_to_play = False
-        self.colour = self.colour_to_bool(colour)
+"""
+Defines the Player class that can be inherited from.
+"""
 
-    def colour_to_bool(self, colour):
-        if isinstance(colour, str):
-            if colour == "white":
-                return True
-            elif colour == "black":
-                return False
-        elif isinstance(colour, bool):
-            return colour
-        elif isinstance(colour, int) and ((0 == colour) or (1 == colour)):
-            return colour
+
+class Player:
+    def __init__(self, board, master, colour, pieces, update, callback,
+                 debug=False):
+        """
+ ---------------- -------------------- ---------------------------------------- 
+| Variable       | Type               | Further info                           |
+ ---------------- -------------------- ---------------------------------------- 
+| self.board     | chess.Board object | Never push moves, call self.callback   |
+| self.master    | tk.Canvas object   | Will be deprecated in the future       |
+| self.tk_object | a tk widget object | Used only for `widget.after()`         |
+| self.colour    | bool               | The colour of the player               |
+| self.pieces    | [piece.Piece, ...] | The pieces on GUIBoard (deprecated)    |
+| self.debug     | bool               | Used only in testing                   |
+| self.callback  | function           | use: `self.callback(move: chess.Move)` |
+| self.update    | function           | use: `self.update(redraw: bool)`       |
+ ---------------- -------------------- ---------------------------------------- 
+        """
+        self.board = board
+        self.master = master
+        self.tk_object = master
+        self.colour = colour
+        self.pieces = pieces
+        self.update = update
+        self.callback = callback
+        self.debug = debug
+
+        self.alowed_to_play = False
 
     def start(self):
         self.alowed_to_play = True
@@ -22,13 +38,35 @@ class Player:
         self.alowed_to_play = False
 
     def destroy(self):
+        """
+            After this method is called the player must stop immediately.
+        """
         self.stop()
 
     def undo_move(self, move):
+        """
+            Called when the user wants to undo a move.
+            If "break" is returned the undo will be blocked
+        """
         pass
 
     def redo_move(self, move):
+        """
+            Called when the user wants to redo a move.
+            If "break" is returned the redo will be blocked
+        """
         pass
 
     def go(self):
+        """
+            Called every time the GUIBoard needs the player to move
+            Always call with the move `self.callback(move)`
+            Only allowed to return move when `self.colour == self.baord.turn`
+        """
         pass
+
+    def colour_to_bool(self, colour):
+        if isinstance(colour, str):
+            return colour == "white"
+        else:
+            return colour
