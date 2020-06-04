4eda0bac502aa9a62602f50cb126d0f838718d14
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-06-04 21:35:55 +0100

    Added comments and method annotations

--- scr/Players/computer.py
+++ scr/Players/computer.py
@@ -1,3 +1,9 @@
+"""
+This implements the Computer class. It uses Stockfish to calculate a move
+from a board position when go is called.
+"""
+
+
 from chess.engine import SimpleEngine, Limit, Mate
 
 from .player import Player
@@ -14,9 +20,12 @@ class Computer(Player):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def go(self):
+    def go(self) -> None:
+        # We don't want any errors because the game is over or we lost
+        # permissions to send moves to GUIBoard
         if (self.board.is_game_over()) or (not self.alowed_to_play):
             return None
+        # Improvement: Add a changing depth and time
         self.limit = Limit(depth=DEPTH, time=TIME)
         with SimpleEngine.popen_uci(STOCKFISH_LOCATION) as engine:
             result = engine.play(self.board, self.limit)
