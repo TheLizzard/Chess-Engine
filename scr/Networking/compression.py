64fdeee1dced580610130c53e89e6245daa6ce19
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-06-04 21:34:20 +0100

    Added comments and method annotations

--- scr/Networking/compression.py
+++ scr/Networking/compression.py
@@ -1,3 +1,30 @@
+"""
+This is a simple module that can compress chess moves and fens
+(will add text as well)
+These are the functions implemented:
+    `compress_move(move: chess.Move) -> Bits`
+    `decompress_move(bits: Bits) -> chess.Move`
+    `compress_fen(fen: str) -> Bits`
+    `decompress_fen(bits: Bits) -> str`
+
+The protocol for compressing moves is:
+ ------------------- ---------------------- ------------- ------------- 
+|  1, 2, 3, 4, 5, 6 |  7, 8, 9, 10, 11, 12 |    13, 14   |    15, 16   |
+ ------------------- ---------------------- ------------- -------------
+|    from_square    |       to_square      | promoting   | checksum    |
+ ------------------- ---------------------- ------------- ------------- 
+|  0 = A1           |                      | 0 = default | sum of all  |
+|  1 = A2           |  same as             | 0 = knight  | other bits  |
+|  2 = A3           |  from_square         | 1 = bishop  | mod 4       |
+|   ...             |                      | 2 = rook    | can be: 00, |
+| 63 = H8           |                      | 3 = queen   | 01, 10, 11  |
+ ------------------- ---------------------- ------------- ------------- 
+Note: numbers on the top represend the bit number
+The checksum is redundant as the TCP packets have their own checksum but
+there is no other use for the 2 bits.
+"""
+
+
 import chess
 import re
 
@@ -8,7 +35,7 @@ from .bits import Bits
 __all__ = ["compress_move",
            "compress_fen",
            "decompress_move",
-           "decompress_fen"]
+           "decompress_fen"] # These are the only ones that should be accessed
 
 
 LETTER_TO_NUMBER = {"p": 0,
@@ -22,27 +49,23 @@ NUMBER_TO_LETTER = {v: k for k, v in LETTER_TO_NUMBER.items()}
 
 
 # Starting compressing move
-def get_check_sum(bits_to_sum, bits=None):
+def get_check_sum(bits_to_sum: Bits, bits=None) -> Bits:
+    """
+    This sums the bits (as a Bits object) and takes the modulo of the number
+    of bits required. It returns a Bits object.
+    """
     sum = str(bits_to_sum).count("1")
     if bits is not None:
         sum %= bits
     return Bits.from_int(sum, bits=bits)
 
-def compress_move(move):
+def compress_move(move: chess.Move) -> Bits:
     """
-    Bits:
-     ------------------- ---------------------- ------------- -------------
-    |  1, 2, 3, 4, 5, 6 |  7, 8, 9, 10, 11, 12 |    13, 14   |    15, 16   |
-     ------------------- ---------------------- ------------- -------------
-    |    from_square    |       to_square      | promoting   | checksum    |
-     ------------------- ---------------------- ------------- -------------
-    |  0 = A1           |                      | 0 = default | sum of all  |
-    |  1 = A2           |  same as             | 0 = knight  | other bits  |
-    |  2 = A3           |  from_square         | 0 = bishop  | mod 4       |
-    |   ...             |                      | 0 = rook    | can be: 00, |
-    | 63 = H8           |                      | 0 = queen   | 01, 10, 11  |
-     ------------------- ---------------------- ------------- -------------
+    This compresses a chess.Move to Bits by using the protocol described
+    on the top of this file:
     """
+    if move == "undo":
+        return Bits.from_int(0, bits=16)
     position1 = Bits.from_int(move.from_square, bits=6)
     position2 = Bits.from_int(move.to_square, bits=6)
     position = position1.concatenate(position2)
@@ -59,11 +82,18 @@ def compress_move(move):
 # Done compressing move
 
 # Starting decompressing move
-def decompress_move(compressed, errors="strict"):
+def decompress_move(compressed: Bits, errors="strict") -> chess.Move:
+    """
+    Takes a compressed move and decompresses it into a chess.Move. It returns
+    None is the data is corrupt. It returns a null chess.Move if the compressed
+    is all 0s.
+    """
     cpos1, cpos2 = compressed[:6], compressed[6:12]
     cpro, check = compressed[12:14], compressed[14:]
     if str(compressed[:14]).count("1")%len(check) != int(check):
-        return "corrupted data"
+        return None
+    if compressed == Bits.from_int(0, bits=16):
+        return chess.Move(from_square=None, to_square=None)
     pos1 = int(cpos1)
     pos2 = int(cpos2)
     pro = int(cpro)+2
@@ -71,7 +101,7 @@ def decompress_move(compressed, errors="strict"):
 # Done decompressing move
 
 # Starting compressing fen
-def _compress_fen(fen):
+def _compress_fen(fen: str) -> Bits:
     result = re.search("(\d+)\/(\d+)", fen)
     if result is not None:
         groups = result.groups()
@@ -80,18 +110,19 @@ def _compress_fen(fen):
         fen = fen[:start]+_compress_fen(number+fen[end:])
     return fen
 
-def compress_fen(fen):
+def compress_fen(fen: str) -> Bits:
     """
-    If there are 2 numbers separated by a "/" add them and remove the "/"
-    The rest of the "/"s are removed.
-    For each character in the modifed FEN string do:
-        Each piece is 5 bits:
-            bits[0] = 1 # to show that it is a piece
-            bits[1] = colour of the piece
-            bits[2:] = piece type (taken from LETTER_TO_NUMBER)
-        Each number is 7 bits:
-            bits[0] = 0 # to show that it is a number not a piece
-            bits[1:] = number
+    This compresses a fen string to a Bits object by using this protocol:
+        If there are 2 numbers separated by a "/" add them and remove the "/"
+        The rest of the "/"s are removed.
+        For each character in the modifed FEN string do:
+            Each piece is 5 bits:
+                bits[0] = 1 # to show that it is a piece
+                bits[1] = colour of the piece
+                bits[2:] = piece type (taken from LETTER_TO_NUMBER)
+            Each number is 7 bits:
+                bits[0] = 0 # to show that it is a number not a piece
+                bits[1:] = number
     """
     fen = _compress_fen(fen).replace("/", "")
     new_fen = Bits()
@@ -120,7 +151,11 @@ def compress_fen(fen):
 # Done compressing fen
 
 # Starting decompressing fen
-def decompress_fen(bits, errors="strict"):
+def decompress_fen(bits: Bits, errors="strict") -> str:
+    """
+    This decompresses a fen from a Bits object to a string. It does the
+    inverse of `compress_fen(fen: str) -> Bits`
+    """
     fen = ""
     while len(bits) > 0:
         if len(bits) < 5:
@@ -144,7 +179,16 @@ def decompress_fen(bits, errors="strict"):
         fen = fen[:-1]
     return fen
 
-def add_slashes(fen, errors="strict"):
+def add_slashes(fen: str, errors="strict") -> str:
+    """
+    This function adds slashes to fens like:
+    "rnbqkbnrpppppppp32PPPPPPPPRNBQKBNR"
+    to make it look like this:
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+
+    It needs to add slashes every 8 squares and needs to keep in mind that
+    sometimes numbers can be more than 8.
+    """
     new_fen = ""
     from_last_slash = 0
     i = 0
