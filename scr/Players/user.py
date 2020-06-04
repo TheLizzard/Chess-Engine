036a06a2b580a3cfbeca1b09b3636b368aa0fc6e
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-06-04 21:36:42 +0100

    Added comments and method annotations

--- scr/Players/user.py
+++ scr/Players/user.py
@@ -38,27 +38,35 @@ class User(Player):
         self.size = BOARD_SETTINGS.size_of_squares
         self.bind_mouse()
 
-    def bind_mouse(self):
+    def bind_mouse(self) -> None:
         """
-            This binds both mouse buttons (left and right) for all 3 events:
-                Press
-                Release
-                Motion
-            All of the events go to `self.mouse`
+        This binds both mouse buttons (left and right) for all 3 events:
+            Press
+            Release
+            Motion
+        All of the events go to `self.mouse`
         """
         for n in (1, 3):
             for i in ("<Button-%d>", "<B%d-Motion>", "<ButtonRelease-%d>"):
                 self.master.bind(i%n, self.mouse, True)
 
-    def destroy(self):
+    def destroy(self) -> None:
+        """
+        Destroys everything this class has created.
+        Improvement: add a way to unbind the tkinter Events
+        """
         self.unselect()
         self.remove_available_moves()
         self.delete_user_created_object()
         self.stop_user_created_object()
+        # can't unbind tkinter events so this must be destroyed
         print("please del me. I can't unbind")
         super().destroy()
 
-    def mouse(self, event):
+    def mouse(self, event: tk.Event) -> None:
+        """
+        This takes in all tkinter mouse events and reacts to them.
+        """
         if (not self.alowed_to_play) or (self.colour != self.board.turn):
             return None
         # name can be one of: (ButtonPress, ButtonRelease, Motion)
@@ -124,7 +132,14 @@ class User(Player):
                     else:
                         self.user_helper_making = self.create_arrow(start, end)
 
-    def create_user_helping_object(self, start, end):
+    def create_user_helping_object(self, start: Postion, end: Postion) -> None:
+        """
+        This creates a object that can help the user like:
+            rings (can be created by clicking the right mouse button)
+            arrows (can be created by dragging the right mouse button)
+        To remove all of the objects from the board eather create a new
+        object over them or click with the left mouse button.
+        """
         if start == end:
             _hash = hash(start*2)
             if _hash in self.user_created_helpers:
@@ -142,7 +157,13 @@ class User(Player):
                 arrow = self.user_helper_making
                 self.user_created_helpers.update({_hash: arrow})
 
-    def delete_user_created_object(self, idx=None):
+    def delete_user_created_object(self, idx: int=None) -> None:
+        """
+        This deletes the objects that help the user like arrows and rings.
+        If idx (must come from `position.Move.__hash__`) is specified
+        only that one will be deleted.
+        Else all of the object will be deleted.
+        """
         if idx is None:
             for _, i in self.user_created_helpers.items():
                 self.master.delete(i)
@@ -151,19 +172,31 @@ class User(Player):
             self.master.delete(self.user_created_helpers[idx])
             del self.user_created_helpers[idx]
 
-    def stop_user_created_object(self):
+    def stop_user_created_object(self) -> None:
+        """
+        Stop the user from creating a helping object.
+        This is fix to Issue #7
+        """
         if self.user_helper_making is not None:
             self.master.delete(self.user_helper_making)
 
-    def create_ring(self, position):
+    def create_ring(self, position: Position) -> None:
+        """
+        Draws a temporaty ring to help the user.
+        """
         coords = position.to_coords()
         colour = USER_SETTINGS.ring_colour
         width = USER_SETTINGS.ring_width
         radius = USER_SETTINGS.ring_radius
+        # `start` and `end` are weird numbers because if start is 0 and end is
+        # 360 tkinter doesn't draw anything.
         return self._create_circle_arc(*coords, style="arc", r=radius, end=629,
                                        outline=colour, width=width, start=270)
 
-    def create_arrow(self, position1, position2):
+    def create_arrow(self, position1: Position, position2: Position) -> None:
+        """
+        Draws a temporaty arrow to help the user.
+        """
         coords1 = position1.to_coords()
         coords2 = position2.to_coords()
         width = USER_SETTINGS.arrow_width
@@ -171,13 +204,20 @@ class User(Player):
         return self.master.create_line(*coords1, *coords2, arrow="last",
                                        fill=colour, width=width)
 
-    def _create_circle_arc(self, x, y, r, **kwargs):
+    def _create_circle_arc(self, x: int, y: int, r: int, **kwargs) -> None:
+        """
+        Used to create an arc. Taken from stackoverflow
+        """
         if "start" in kwargs and "end" in kwargs:
             kwargs["extent"] = kwargs["end"] - kwargs["start"]
             del kwargs["end"]
         return self.master.create_arc(x-r, y-r, x+r, y+r, **kwargs)
 
-    def move_selected(self, new_coords):
+    def move_selected(self, new_coords: tuple) -> None:
+        """
+        Moves the selected piece to the new_coords (the argument).
+        It checks if the move is legal.
+        """
         new_position = Position.from_coords(new_coords)
         old_position = self.piece_selected.position
 
@@ -196,31 +236,42 @@ class User(Player):
             self.unselect()
         self.update()
 
-    def push(self, move):
-        if self.callback is not None:
-            self.callback(move)
+    def push(self, move: chess.Move) -> None:
+        """
+        Calls callback with the move as by the board, player protocol
+        """
+        self.callback(move)
 
-    def select(self, position):
+    def select(self, position: Position) -> None:
+        """
+        Selects the piece the user has clicked.
+        """
         piece_selected = self.position_to_piece(position)
         if piece_selected is not None:
             colour = self.colour_to_bool(piece_selected.colour)
             if colour == self.board.turn:
                 self.piece_selected = piece_selected
                 self.show_available_moves()
-                return True
+                return None
         self.unselect()
-        return False
 
-    def position_to_piece(self, position):
+    def position_to_piece(self, position: Position) -> Piece:
         for piece in self.pieces:
             if piece.position == position:
                 return piece
 
     def unselect(self):
+        """
+        Unselects the selected piece
+        """
         self.piece_selected = None
         self.remove_available_moves()
 
-    def show_available_moves(self):
+    def show_available_moves(self) -> None:
+        """
+        Show the available moves by creating small dots on the board where
+        there are legal moves
+        """
         piece = self.piece_selected
         for move in self.board.legal_moves:
             if move.from_square == piece.position.to_int():
@@ -228,11 +279,11 @@ class User(Player):
                 if self.position_to_piece(position) is None:
                     self.draw_available_move(position)
 
-    def remove_available_moves(self):
+    def remove_available_moves(self) -> None:
         for dot in self.available_move_dots:
             self.master.delete(dot)
 
-    def draw_available_move(self, position):
+    def draw_available_move(self, position: Position) -> None:
         radius = self.size/9
         colour = USER_SETTINGS.available_moves_dots_colour
         dot = self.draw_dot(position, radius, outline=colour, fill=colour)
@@ -243,14 +294,14 @@ class User(Player):
         r = radius
         return self.master.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 
-    def legal_promoting(self, uci, new_position):
+    def legal_promoting(self, uci: str, new_position: Position) -> bool:
         if self.piece_selected.name == "pawn":
             if (new_position[1] == 8) or (new_position[1] == 1):
                 potential_move = chess.Move.from_uci(uci+"q")
                 return (potential_move in self.board.legal_moves)
         return False
 
-    def askuser_pawn_promotion(self):
+    def askuser_pawn_promotion(self) -> str:
         self.stop()
         master = self.master
         self.chosen_promotion = None
@@ -276,22 +327,22 @@ class User(Player):
         self.start()
         return self.chosen_promotion
 
-    def promote(self, event):
+    def promote(self, event: tk.Event) -> None:
         promotions = ("q", "r", "b", "n")
         position = Position.from_coords((event.x, event.y))
         if position[1] == 4:
             if 3 <= position[0] <= 6:
                 self.chosen_promotion = promotions[position[0]-3]
 
-    def undo_move(self, move):
+    def undo_move(self, move: chess.Move) -> str:
         self.remove_available_moves()
         self.stop_user_created_object()
 
-    def redo_move(self, move):
+    def redo_move(self, move chess.Move) -> str:
         self.remove_available_moves()
         self.stop_user_created_object()
 
-    def create_piece(self, **kwargs):
+    def create_piece(self, **kwargs) -> Piece:
         kwargs.update({"master": self.master})
         piece = Piece(**kwargs)
         piece.resize_scale(scale=BOARD_SETTINGS.scale_for_pieces)
