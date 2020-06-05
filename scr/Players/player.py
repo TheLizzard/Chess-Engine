2e3df500471eac7d3c2bbbf54cf923f9177a0942
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-06-05 20:37:21 +0100

    Added undo and redo move to the board player protocol

--- scr/Players/player.py
+++ scr/Players/player.py
@@ -8,28 +8,30 @@ import chess
 
 class Player:
     def __init__(self, board, master, colour, pieces, update, callback,
-                 debug=False):
+                 request_undo, request_redo, debug=False):
         """
- ---------------- -------------------- ---------------------------------------- 
-| Variable       | Type               | Further info                           |
- ---------------- -------------------- ---------------------------------------- 
-| self.board     | chess.Board object | Never push moves, call self.callback   |
-| self.master    | tk.Canvas object   | Will be deprecated in the future       |
-| self.tk_object | a tk widget object | Used only for `widget.after()`         |
-| self.colour    | bool               | The colour of the player               |
-| self.pieces    | [piece.Piece, ...] | The pieces on GUIBoard (deprecated)    |
-| self.debug     | bool               | Used only in testing                   |
-| self.callback  | function           | use: `self.callback(move: chess.Move)` |
-| self.update    | function           | use: `self.update(redraw: bool)`       |
- ---------------- -------------------- ---------------------------------------- 
+ -------------- -------------------- ---------------------------------------- 
+| Variable     | Type               | Further info                           |
+ -------------- -------------------- ---------------------------------------- 
+| board        | chess.Board object | Never push moves, call self.callback   |
+| master       | tk.Canvas object   |                                        |
+| colour       | bool               | The colour of the player               |
+| pieces       | [piece.Piece, ...] | The pieces on GUIBoard                 |
+| debug        | bool               | Used only in testing                   |
+| request_undo | function           | used to requst an undo                 |
+| request_redo | function           | used to requst an redo                 |
+| callback     | function           | use: `self.callback(move: chess.Move)` |
+| update       | function           | use: `self.update(redraw: bool)`       |
+ -------------- -------------------- ---------------------------------------- 
         """
         self.board = board
         self.master = master
-        self.tk_object = master
         self.colour = colour
         self.pieces = pieces
         self.update = update
         self.callback = callback
+        self.request_undo = request_undo
+        self.request_redo = request_redo
         self.debug = debug
 
         self.alowed_to_play = False
@@ -42,34 +44,42 @@ class Player:
 
     def destroy(self) -> None:
         """
-            After this method is called the player must stop immediately.
+        After this method is called the player must stop immediately.
         """
         self.stop()
 
     def undo_move(self, move: chess.Move) -> str:
         """
-            Called when the user wants to undo a move.
-            If "break" is returned the undo will be blocked
+        Called when the user wants to undo a move.
+        If "break" is returned the undo will be blocked
         """
         pass
 
     def redo_move(self, move: chess.Move) -> str:
         """
-            Called when the user wants to redo a move.
-            If "break" is returned the redo will be blocked
+        Called when the user wants to redo a move.
+        If "break" is returned the redo will be blocked
         """
         pass
 
     def go(self) -> None:
         """
-            Called every time the GUIBoard needs the player to move
-            Always call with the move `self.callback(move)`
-            Only allowed to return move when `self.colour == self.baord.turn`
+        Called every time the GUIBoard needs the player to move
+        Always call with the move `self.callback(move)`
+        Only allowed to return move when `self.colour == self.baord.turn`
         """
         pass
 
-    def colour_to_bool(self, colour) -> bool:
-        if isinstance(colour, str):
-            return colour == "white"
-        else:
-            return colour
+    def undo_move(self, move: chess.Move):
+        """
+        This is called when the other player wants to undo a move. If
+        `"break"` is returned the undo process will stop.
+        """
+        return None
+
+    def redo_move(self, move: chess.Move):
+        """
+        This is called when the other player wants to redo a move. If
+        `"break"` is returned the redo process will stop.
+        """
+        return None
