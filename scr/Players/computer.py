1f642ebb09f9f6c7293ac8a1f665b7e7ced62f7f
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-06-16 11:44:16 +0100

    Fix for Issue #20

--- scr/Players/computer.py
+++ scr/Players/computer.py
@@ -7,13 +7,23 @@ from a board position when go is called.
 from chess.engine import SimpleEngine, Limit, Mate
 
 from .player import Player
-import settings
 
-settings = settings.Settings()
-STOCKFISH_LOCATION = settings.evaluation.stockfish
-DEPTH = settings["gameboard.computer"].depth
-TIME = settings["gameboard.computer"].time
-del settings
+
+"""
+Get the correct Stockfish file that the OS can use. Fix for Issue #20.
+    stockfish_11_x64      # For Linux 64 bit
+    stockfish_11_x32.exe  # For Windows 32 bit
+    stockfish_11_x64.exe  # For Windows 64 bit
+"""
+import settings
+# Get the folder of all of the Sockfishes
+STOCKFISH_FOLDER = settings.Settings().evaluation.stockfish
+os_bits = str(settings.get_os_bits()) # Get the bit version of the OS
+# Get the file extension that the OS supports
+os_extension = settings.get_os_extension()
+# Combine everything to get the location
+STOCKFISH_LACATION = STOCKFISH_FOLDER+os_bits+os_extension
+del settings, os_bits, os_extension, STOCKFISH_FOLDER # clean up
 
 
 class Computer(Player):
