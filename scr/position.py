a613f6e9b9479668878c62719a555d7694521a2d
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-06-17 21:07:27 +0100

    Fix for Issue #28
    Removed a lot of temporary variables that can slow down the code.

--- scr/position.py
+++ scr/position.py
@@ -1,4 +1,21 @@
 #https://codegolf.stackexchange.com/questions/63772/determine-the-color-of-a-chess-square
+
+"""
+This module is the core of the whole program. This does all
+of the calculations between different ways of saying chess position.
+For example all of these are the same place (assuming the size
+of a square = 60):
+    as a place         A8
+    as an int          56
+    as x and y         1, 8
+    as coords          (30, 30)
+    as coords (start)  (0, 0)
+    as coords (end)    (60, 60)
+
+This library is called a lot of times so it is very important to
+keep it as fast and clean as possible.
+"""
+
 import chess
 
 from settings import Settings
@@ -13,13 +30,10 @@ class Move:
         self.position2 = position2
 
     def to_str(self) -> str:
-        place1 = self.position1.to_place()
-        place2 = self.position2.to_place()
-        return place1 + place2
+        return self.position1.to_place() + self.position2.to_place()
 
     def __hash__(self) -> int:
-        string = str(self.position1.to_int())+str(self.position2.to_int())
-        return int(string)
+        return int(str(self.position1.to_int())+str(self.position2.to_int()))
 
 
 class Position:
@@ -41,19 +55,11 @@ class Position:
             raise ValueError(repr(other)+"has to be an instance of: <int>")
 
     def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            if self.x == other.x:
-                if self.y == other.y:
-                    return True
-        return False
-
-    def __ne__(self, other): # easier to port to python 2 in the future
-        return not (self == other)
+        return isinstance(other, self.__class__) and (self.x == other.x)\
+               and (self.y == other.y)
 
     def __repr__(self):
-        out = "<Position object at "+hex(id(self))
-        out += " x="+str(self.x)+" y="+str(self.y)+">"
-        return out
+        return f"<Position object at {hex(id(self))} x={self.x} y={self.y}>"
 
     def __getitem__(self, key: int) -> int:
         if isinstance(key, int):
@@ -69,19 +75,13 @@ class Position:
         return self.to_int()
 
     def to_coords(self) -> tuple:
-        x = (self.x-0.5)*SIZE
-        y = (8.5-self.y)*SIZE
-        return (self.round(x), self.round(y))
+        return (int((self.x-0.5)*SIZE+0.5), int((8.5-self.y)*SIZE+0.5))
 
     def to_coords_start(self) -> tuple:
-        x = (self.x-1)*SIZE
-        y = (8-self.y)*SIZE
-        return (self.round(x), self.round(y))
+        return (int((self.x-1)*SIZE+0.5), int((8-self.y)*SIZE+0.5))
 
     def to_coords_end(self) -> tuple:
-        x = (self.x)*SIZE
-        y = (9-self.y)*SIZE
-        return (self.round(x), self.round(y))
+        return (int((self.x)*SIZE+0.5), int((9-self.y)*SIZE+0.5))
 
     def to_place(self) -> str:
         return chess.FILE_NAMES[self.x-1]+str(self.y)
@@ -92,19 +92,11 @@ class Position:
     def to_colour(self) -> bool:
         return bool(int(self.to_place(), 35)%2) # Converts to base 35 first.
 
-    @staticmethod
-    def round(x: int) -> int:
-        return int(x+0.5)
-
     @classmethod
     def from_coords(cls, coords: tuple):
         x, y = coords
-        new_x = x//SIZE+1
-        new_y = (SIZE*8-y)//SIZE+1
-        return cls(Position.round(new_x), Position.round(new_y))
+        return cls(int(x//SIZE+1+0.5), int((SIZE*8-y)//SIZE+1+0.5))
 
     @classmethod
     def from_int(cls, _int: int):
-        x = _int%8+1
-        y = _int//8+1
-        return cls(x, y)
+        return cls(_int%8+1, _int//8+1)
