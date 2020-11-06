3b722231e79414bb2a68c02ebffe8431049f96a6
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2020-11-06 13:31:56 +0000

    Added some comments

--- scr/reset_app.py
+++ scr/reset_app.py
@@ -1,5 +1,8 @@
+# Use the updater to reset the app by ckearing files.txt
 import Networking.updater as updater
 with open("files.txt", "w") as file:
     file.write("")
 updated = updater.update()
+
+# Start the updated program
 import main
